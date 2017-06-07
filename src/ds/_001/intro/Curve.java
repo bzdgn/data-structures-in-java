e5818497d3b664593f0ba7c6ad388bc96421c43f
Levent Divilioglu <divilioglu@gmail.com>
2017-06-08 01:11:22 +0300

    rename

--- src/ds/_001/intro/Curve.java
+++
@@ -1,7 +0,0 @@
-package ds._001.intro;
-
-public interface Curve {
-	
-	double getValue(double x);
-	
-}
---
+++ src/ds_001/intro/Curve.java
@@ -0,0 +1,7 @@
+package ds_001.intro;
+
+public interface Curve {
+	
+	double getValue(double x);
+	
+}
